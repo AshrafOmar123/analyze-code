700b907289f0b3bf43003ffaffff2dfa94399149
Ashraf AlHinai <129645230+AshrafOmar123@users.noreply.github.com>
2024-04-07 22:13:36 +0400

    Update original.py

--- original.py
+++ original.py
@@ -1,1 +1,25 @@
-import mathdef main():        word = "Be Safe"        for i in range(8):        print(word[i])        result = math.exp(2000)    print(result)        result2 = factorial(20000)    print(result2)    def factorial(n):    if n == 0:        return 1        else:        return n * factorial(n - 1)        main()
+import math
+
+
+def main():
+    
+    word = "Be Safe"
+    
+    for i in range(8):
+        print(word[i])
+    
+    result = math.exp(2000)
+    print(result)
+    
+    result2 = factorial(20000)
+    print(result2)
+    
+def factorial(n):
+    if n == 0:
+        return 1
+    
+    else:
+        return n * factorial(n - 1)
+    
+    
+main()
